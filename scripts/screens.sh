01c356368800c8420970a81a2c0c48d617976594
Greg Leonard <gr3gl@hotmail.com>
2020-04-04 17:05:15 +0100

    updates

--- scripts/screens.sh
+++ scripts/screens.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-I-0 --off --output DVI-I-1 --off --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate left --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --mode 2560x1440 --pos 1080x240 --rotate normal --output DP-5 --off
+xrandr --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate left --output DP-0 --primary --mode 2560x1440 --rate 155.00 --pos 1080x240 --rotate normal 
